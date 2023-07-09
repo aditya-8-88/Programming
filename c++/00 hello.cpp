cdf9c21113d95c685dacf3aef7994480cca69703
Aditya Pratap Singh <113544669+APS-coder@users.noreply.github.com>
2023-07-09 06:50:28 +0530

    Add files via upload

---
+++ c++/00 hello.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "hello world" << endl;
+//     // cout<<"this line";
+//     int a = 5, b = 7;
+//     short c = 9;
+//     cout << "a=" << a << endl
+//          << "b=" << b << endl
+//          << "c=" << c << endl;
+//     cout << "Enter a number:";
+//     cin >> a;
+//     cout << "sum = " << (float)a / b;
+    
+
+//     return 0;
+// }
