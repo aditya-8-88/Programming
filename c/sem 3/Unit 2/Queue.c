092684937484551e4335e96399874de2dfc60582
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2024-02-06 01:29:43 +0530

    new file

--- c/sem 3/Unit 2/Queue.c
+++ c/sem 3/Unit 2/Queue.c
@@ -1,77 +1,164 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #define MAX_SIZE 10
+
+// int front = -1, rear = -1;
+// int capacity = MAX_SIZE;
+// int array[MAX_SIZE];
+
+// int isEmpty() {
+//     return (front == -1);
+// }
+
+// int isFull() {
+//     return (rear == capacity - 1);
+// }
+
+// void insert(int item) {
+//     if (isFull()) {
+//         printf("Queue is full. Cannot enqueue.\n");
+//         return;
+//     }
+
+//     if (isEmpty()) {
+//         front = 0;
+//     }
+
+//     rear++;
+//     array[rear] = item;
+//     printf("%d enqueued to the queue.\n", item);
+// }
+
+// int delete() {
+//     int item;
+
+//     if (isEmpty()) {
+//         printf("Queue is empty. Cannot delete.\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     item = array[front];
+
+//     if (front == rear) {
+//         front = rear = -1;
+//     } else {
+//         front++;
+//     }
+
+//     return item;
+// }
+
+// void display() {
+//     int i;
+//     if (isEmpty()) {
+//         printf("Queue is empty.\n");
+//         return;
+//     }
+
+//     printf("Queue elements: ");
+//     for (i = front; i <= rear; i++) {
+//         printf("%d ", array[i]);
+//     }
+//     printf("\n");
+// }
+
+// int main() {
+//     insert(1);
+//     insert(2);
+//     insert(3);
+
+//     display();
+
+//     printf("deleted element: %d\n", delete());
+
+//     display();
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *front;
+struct node *rear;
 
-#define MAX_SIZE 10
 
-int front = -1, rear = -1;
-int capacity = MAX_SIZE;
-int array[MAX_SIZE];
+void insert(int item)
+{
+    struct node *ptr;
 
-int isEmpty() {
-    return (front == -1);
-}
-
-int isFull() {
-    return (rear == capacity - 1);
-}
-
-void insert(int item) {
-    if (isFull()) {
-        printf("Queue is full. Cannot enqueue.\n");
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW\n");
         return;
     }
-
-    if (isEmpty()) {
-        front = 0;
+    else
+    {
+        ptr->data = item;
+        if (front == NULL)
+        {
+            front = ptr;
+            rear = ptr;
+            front->next = NULL;
+            rear->next = NULL;
+        }
+        else
+        {
+            rear->next = ptr;
+            rear = ptr;
+            rear->next = NULL;
+        }
     }
-
-    rear++;
-    array[rear] = item;
-    printf("%d enqueued to the queue.\n", item);
 }
-
-int delete() {
-    int item;
-
-    if (isEmpty()) {
-        printf("Queue is empty. Cannot delete.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    item = array[front];
-
-    if (front == rear) {
-        front = rear = -1;
-    } else {
-        front++;
-    }
-
-    return item;
-}
-
-void display() {
-    int i;
-    if (isEmpty()) {
-        printf("Queue is empty.\n");
+void delete()
+{
+    struct node *ptr;
+    if (front == NULL)
+    {
+        printf("\nUNDERFLOW\n");
         return;
     }
-
-    printf("Queue elements: ");
-    for (i = front; i <= rear; i++) {
-        printf("%d ", array[i]);
+    else
+    {
+        ptr = front;
+        front = front->next;
+        free(ptr);
     }
-    printf("\n");
+}
+void display()
+{
+    struct node *ptr;
+    ptr = front;
+    if (front == NULL)
+    {
+        printf("\nEmpty queue\n");
+    }
+    else
+    {
+        while (ptr != NULL)
+        {
+            printf("%d ", ptr->data);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
 }
 
-int main() {
-    insert(1);
-    insert(2);
-    insert(3);
-
+int main()
+{
     display();
-
-    printf("deleted element: %d\n", delete());
-
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40);
+    display();
+    delete ();
     display();
 
     return 0;
