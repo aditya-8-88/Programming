f16f681093ed95b8b44fd2251b6bb13691f31cea
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2024-06-15 23:09:36 +0530

    modified

---
+++ Java/college/self/Hello.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Hello{
+    public static void main(String[] args){
+        int[] arr = new int[5];
+        Arrays.
+        System.out.println("Hello World");
+    }
+}
