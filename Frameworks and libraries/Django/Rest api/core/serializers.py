18d02590960c89fbccb88a0da259602512b52a90
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2024-11-28 10:15:39 +0530

    deleted several learning project to create new repo

--- Frameworks and libraries/Django/Rest api/core/serializers.py
+++
@@ -1,9 +0,0 @@
-# python object to json object
-
-from rest_framework import serializers
-from .models import Drink
-
-class DrinkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Drink
-        fields = '__all__'
--- Frameworks and libraries/Django/Rest api/readme.md
+++
@@ -1,5 +0,0 @@
-# Info
-
-1. Exploring rest api in Django
-2. start mysql at the default port(3306)
-2. connecting mysql localhost to Django 
