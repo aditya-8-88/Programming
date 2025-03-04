759b8ca30d8a0ea9afa3bacd77092f48f318dafc
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2025-03-04 18:29:58 +0530

    Typescript practise

---
+++ Languages/TypeScript/index.ts
@@ -0,0 +1,17 @@
+console.log('Hello, world!');
+
+let age : number = 30;
+// age = "fdf";
+let nname = 'John Doe';
+let isAdult = true;
+let hobbies : string[] = ['reading', 'coding', 'gaming'];
+let address : object = {
+    street: '123 Main St',
+    city: 'New York',
+    state: 'NY'
+};
+let phone : any = 1234567890;
+
+
+
+hobbies.forEach( n => console.log(n.charAt(0)));
